2da0ca5cff63f09cbaa615a85ae5202ced7f8982
Eric Fithian <ef2413@columbia.edu>
2022-01-12 01:48:12 -0500

    Updated to basics

--- controllers/product_controller.js
+++
@@ -1,9 +0,0 @@
-app.get('/products/:productId', (req, res) => {
-
-    products.findById(req.params.productId, (error, foundItem) => {
-        if (error) return console.log(error);
-    
-        return res.render('show.ejs');
-    });
-
-});
