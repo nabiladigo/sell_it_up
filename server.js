1cf60c44b41805bac9741670d73281f790f4d5ca
Eric Fithian <ef2413@columbia.edu>
2022-01-12 11:19:02 -0500

    Updated through the URLs

--- server.js
+++ server.js
@@ -7,7 +7,8 @@ const PORT = 4000;
 const products = [
     {product: "Shirt", price: 20},
     {product: "Shoes", price: 60},
-    {product: "Pants", price: 100}
+    {product: "Pants", price: 100},
+    {product: "Fancy suit", price: 1000}
 ]
 
 // Routes
@@ -16,11 +17,19 @@ app.get("/", function(req, res) {
 })
 
 app.get("/products", function(req, res) {
-    res.send(products)
+    res.send(products[req.query.id])
+})
+
+app.get("/add", function(req, res) {
+    let sum = parseInt(req.query.x) + parseInt(req.query.y);
+    res.send(`If we add ${req.query.x} and ${req.query.y}, the sum will be ${sum}`)
 })
 
 app.get("/products/:index", function(req, res) {
-    res.send(products[req.params.index])
+    res.send(
+        `<h1>Would you like to buy this cool ${products[req.params.index].product}</h1>
+        <h3>Price: $${products[req.params.index].price}</h3>`
+        )
 })
 
 
