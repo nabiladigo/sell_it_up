685c26286a098b0211bad75ae70fe3e71f9aac3a
Eric Fithian <ef2413@columbia.edu>
2022-01-13 16:48:08 -0500

    Updated through creating new info

--- models/product_model.js
+++ models/product_model.js
@@ -57,6 +57,29 @@ class Collection {
     
         return callBack(error, item);
     }
+
+    create( data, callBack ) {
+        if (!data) return console.log("missing data in first argument");
+    
+        if (typeof callBack !== "function") {
+            return console.log("missing function in second argument");
+        }
+    
+        let error, newItem;
+    
+        const isEmpty = Object.keys(data).every(field => data[field] === "");
+    
+        if (isEmpty) {
+            error = { message: `you have empty fields` };
+        } else {
+            
+            newItem = new this.#Model( data, this.#generateId());
+    
+            this.#items[newItem.id] = newItem;
+        }
+    
+        return callBack(error, newItem);
+    }
 };
 
 class Product {
--- server.js
+++ server.js
@@ -2,24 +2,30 @@
 const express = require('express');
 const app = express();
 const products = require('./models/product_model.js');
-
+// const allProducts = products.find();
 // My variables
 const PORT = 4000;
 
 // Set our app
 app.set('view engine', 'ejs');
 
-// Configure it to look in public
+// App.use for adding 
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static('public'));
 
-// Routes
+app.use((req, res, next) => {    
+    // console.log("I'm running for another new route")
+	// console.log(`${req.method} ${req.originalUrl}`);    
+	next();
+});
 
+// Routes
 
 app.get("/", function(req, res) {
     res.send("This is working! :)")
 })
 
-app.get('/products/', (req, res) => {
+app.get('/products', (req, res) => {
     
     const allProducts = products.find();
     /* 
@@ -29,16 +35,30 @@ app.get('/products/', (req, res) => {
     2. the second param is the data that we want 
     to inject into the .ejs file (it must be an object)
     */
-   
-   /*	
-   there will be a variable available inside
-   the show.ejs file called product, 
-   and its value the foundItem
+
+    /*	
+    there will be a variable available inside
+    the show.ejs file called product, 
+    and its value the foundItem
    */
-  const context = { products: allProducts };
-  res.render('index.ejs', context);
-  
+    const context = { products: allProducts };
+    res.render('index.ejs', context);
+
 });
+
+app.post('/products', (req, res) => {
+    products.create(req.body, (error, createdProduct) => {
+        if(error) console.log(error);
+        console.log(createdProduct);
+
+
+        res.redirect("/products");
+    })
+})
+
+app.get("/products/new", function(req, res) {
+    res.render("new.ejs")
+})
 
 // show route
 // this route will catch GET requests to /products/index/ and respond with a single product
@@ -47,8 +67,8 @@ app.get('/products/:productId', (req, res) => {
     products.findById(req.params.productId, (error, foundProduct) => {
         if (error) {
             console.log(error);
-            req.error = error;
-            return next();
+            // req.error = error;
+            // return next();
         }
         /* 
         1. the first param of render() is the .ejs file 
