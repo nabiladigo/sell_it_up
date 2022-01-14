918ca29baa1d5351e3c41a515ad7f33e10255c71
Eric Fithian <ef2413@columbia.edu>
2022-01-14 11:54:38 -0500

    Updated through the products controller work

---
+++ controllers/product_controller.js
@@ -0,0 +1,70 @@
+const express = require('express');
+const router = express.Router();
+const products = require('../models/product_model.js')
+
+router.get('/', (req, res) => {
+    
+    const allProducts = products.find();
+    /* 
+    1. the first param of render() is the .ejs file 
+    that we want to inject data into.
+    
+    2. the second param is the data that we want 
+    to inject into the .ejs file (it must be an object)
+    */
+
+    /*	
+    there will be a variable available inside
+    the show.ejs file called product, 
+    and its value the foundItem
+   */
+    const context = { products: allProducts };
+    res.render('index.ejs', context);
+
+});
+
+router.post('/', (req, res) => {
+    // Start by console logging things out here for the req, then req.body
+    products.create(req.body, (error, createdProduct) => {
+        if(error) console.log(error);
+        console.log(createdProduct);
+
+
+        res.redirect("/products");
+    })
+})
+
+router.get("/new", function(req, res) {
+    res.render("new.ejs")
+})
+
+// show route
+// this route will catch GET requests to /products/index/ and respond with a single product
+router.get('/:productId', (req, res) => {
+    
+    products.findById(req.params.productId, (error, foundProduct) => {
+        if (error) {
+            console.log(req.params)
+            console.log(error);
+            const context = { error: error };
+            return res.status(404).render("404", context);
+        }
+        /* 
+        1. the first param of render() is the .ejs file 
+        that we want to inject data into.
+        
+        2. the second param is the data that we want 
+        to inject into the .ejs file (it must be an object)
+        */
+
+        /*	
+        there will be a variable available inside
+        the show.ejs file called product, 
+        and its value the foundItem
+       */
+        res.render('show.ejs', {product: foundProduct});
+    });
+    
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 // Any imports
 const express = require('express');
 const app = express();
-const products = require('./models/product_model.js');
+const productsController = require('./controllers/product_controller.js')
+
 // const allProducts = products.find();
 // My variables
 const PORT = 4000;
@@ -13,6 +14,8 @@ app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static('public'));
 
+app.use('/products', productsController)
+
 app.use((req, res, next) => {    
     console.log("I'm running for another new route")
 	console.log(`${req.method} ${req.originalUrl}`);    
@@ -20,74 +23,9 @@ app.use((req, res, next) => {
 });
 
 // Routes
-
 app.get("/", function(req, res) {
     res.send("This is working! :)")
 })
-
-app.get('/products', (req, res) => {
-    
-    const allProducts = products.find();
-    /* 
-    1. the first param of render() is the .ejs file 
-    that we want to inject data into.
-    
-    2. the second param is the data that we want 
-    to inject into the .ejs file (it must be an object)
-    */
-
-    /*	
-    there will be a variable available inside
-    the show.ejs file called product, 
-    and its value the foundItem
-   */
-    const context = { products: allProducts };
-    res.render('index.ejs', context);
-
-});
-
-app.post('/products', (req, res) => {
-    // Start by console logging things out here for the req, then req.body
-    products.create(req.body, (error, createdProduct) => {
-        if(error) console.log(error);
-        console.log(createdProduct);
-
-
-        res.redirect("/products");
-    })
-})
-
-app.get("/products/new", function(req, res) {
-    res.render("new.ejs")
-})
-
-// show route
-// this route will catch GET requests to /products/index/ and respond with a single product
-app.get('/products/:productId', (req, res) => {
-    
-    products.findById(req.params.productId, (error, foundProduct) => {
-        if (error) {
-            console.log(error);
-            // req.error = error;
-            // return next();
-        }
-        /* 
-        1. the first param of render() is the .ejs file 
-        that we want to inject data into.
-        
-        2. the second param is the data that we want 
-        to inject into the .ejs file (it must be an object)
-        */
-
-        /*	
-        there will be a variable available inside
-        the show.ejs file called product, 
-        and its value the foundItem
-       */
-        res.render('show.ejs', {product: foundProduct});
-    });
-    
-});
         
 app.get("/*", (req, res) => {
     const context = { error: req.error };
